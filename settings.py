0b2dcd09a4591f8ffa92933a42b360f1e9539f51
gmctl <meigea@0528@gmail.com>
2019-05-13 12:50:22 +0800

    client-upload

--- settings.py
+++ settings.py
@@ -8,12 +8,12 @@ LoggerSettings = dict(
 MongoConfig = dict(host='192.168.2.41', port=27017, db_name='fluent', username=None, password=None)
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'p3',
-            'USER': 'root',
-            'PASSWORD': 'test@1q2w2e4R',
-            'HOST': "192.168.2.55",
-            'PORT': 3306,
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'p3',
+        'USER': 'root',
+        'PASSWORD': 'test@1q2w2e4R',
+        'HOST': "192.168.2.55",
+        'PORT': 3306,
     }
+}
